ae764574ca86299108fdb2038333da525ecb3174
paulorsouza <paulor1809@gmail.com>
2022-10-21 21:09:39 -0300

    Beautify the stopwatch on the console

--- Program.cs
+++ Program.cs
@@ -1,19 +1,22 @@
-﻿using System.Diagnostics;
+﻿using StudyGroupConsoleUI;
+using System.Diagnostics;
 
-Console.ForegroundColor = ConsoleColor.White;
-Console.BackgroundColor = ConsoleColor.Red;
-Console.WriteLine( "Start ");
 
-Console.ForegroundColor = ConsoleColor.Black;
-Console.BackgroundColor = ConsoleColor.DarkGreen;
+// Console.ForegroundColor = ConsoleColor.White;
+// Console.BackgroundColor = ConsoleColor.Red;
+var drawer = new TimerDrawer();
+drawer.drawFrame();
+drawer.drawType();
 
-var tempo = 30 * 1000 * 60;
-var cronometro = Stopwatch.StartNew();
-var percorrido = tempo - cronometro.ElapsedMilliseconds;
+var timer = 30 * 1000 * 60;
+var stopwatch = Stopwatch.StartNew();
+var elapsedTime = timer - stopwatch.ElapsedMilliseconds;
 
-while(percorrido > 0)
-{   
-    var timespan = TimeSpan.FromMilliseconds(percorrido);
-    Console.Write("\r {0:D2}:{1:D2}:{2:D2} ", timespan.Hours, timespan.Minutes, timespan.Seconds);
-    percorrido = tempo - cronometro.ElapsedMilliseconds;
+while(elapsedTime > 0)
+{
+    var timespan = TimeSpan.FromMilliseconds(elapsedTime);
+    var str = string.Format("{0:D2}:{1:D2}:{2:D2}", timespan.Hours, timespan.Minutes, timespan.Seconds);
+    drawer.draw(str);
+    elapsedTime = timer - stopwatch.ElapsedMilliseconds;
 }
+
